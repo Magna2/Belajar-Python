2e2d4c1ee8e41b3d3ac711f46c0363cf24e779eb
Agung Pri <89292313+Magna2@users.noreply.github.com>
2022-05-17 20:41:11 +0700

    Add files via upload

---
+++ Dataset anggota keluarga.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+pizza = {'Nama': ['Wahyu', 'Ilhan', 'Sugeng', 'Agri', 'Johar'],
+'Nilai': [90, 80, 86, 92, 100]
+}
+
+pizza_df = pd.DataFrame(pizza)
+pizza_df
+print (pizza_df)
